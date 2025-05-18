ee666f1419bb77002914735ae37748a1b9ceec4e
Adham Basheir <adhambasheir000@gmail.com>
2025-05-18 19:35:40 +0300

    trail-1

--- terraform/prometheus/main.tf
+++ terraform/prometheus/main.tf
@@ -3,7 +3,7 @@ resource "helm_release" "prometheus" {
   repository       = "https://prometheus-community.github.io/helm-charts"
   chart            = "prometheus"
   namespace        = "monitoring"
-  version          = "58.6.0"
+  version          = "25.21.0"
   create_namespace = true
 
   values = [
